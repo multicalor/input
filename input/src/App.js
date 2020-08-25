7f2fc0b9dedb9710c0a8e8ee7e3174ec6fd50ebf
multicalor <d.tretiuk@gmail.com>
2020-08-26 00:41:11 +0300

    input

---
+++ input/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+import './App.css';
+import Input from './container/inputOne/input'
+
+function App() {
+  return (
+    <div className="App">
+      <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ input/src/container/inputOne/input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Input() {
+    
+  return (
+    <div className="input">
+      <input type='radio' id='start' />
+    </div>
+  );
+}
+
+export default Input;
